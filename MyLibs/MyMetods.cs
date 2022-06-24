69e035f92b9f6a8c63f897e4f3a82354cbcd362c
Sergey Filippenkov <fsvmxd@yandex.ru>
2022-06-24 23:31:30 +0300

    Решение задачи 41

--- MyLibs/MyMetods.cs
+++ MyLibs/MyMetods.cs
@@ -1,5 +1,14 @@
 ﻿namespace MyLibs;
 public class MyMetods
 {
-
+   public static int[] GetArrayFromString(string? arrayStr)
+   {
+      string[] mass = arrayStr!.Split(new char[] { ' ', ',', '*' }, StringSplitOptions.RemoveEmptyEntries);
+      int[] result = new int[mass.Length];
+      for (int i = 0; i < mass.Length; i++)
+      {
+         result[i] = Convert.ToInt32(mass[i]);
+      }
+      return result;
+   }
 }
--- Task-41/Program.cs
+++ Task-41/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+using System;
+using static System.Console;
+using MyLibs;
+Clear();
+
+Write("Введите числа через пробел: ");
+int[] arr = MyMetods.GetArrayFromString(ReadLine());
+
+WriteLine($"Вы ввели {PositiveNumbers(arr)} чисел больше 0");
+
+int PositiveNumbers(int[] array)
+{
+   int count = 0;
+   for (int i = 0; i < array.Length; i++)
+   {
+      if (array[i] > 0) count++;
+   }
+   return count;
+}
